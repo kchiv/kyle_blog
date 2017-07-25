e51b79027d7a7150a73cd4657f92cd7dcf50b0df
Kyle Chivers <chiverskyle@gmail.com>
2017-07-24 19:23:21 -0700

    created post app

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def post_details(request, post_id, slug):
+	post = get_object_or_404(Post, pk=post_id, slug=slug)
+	return render(request, 'posts/post_detail.html', {'post':post})

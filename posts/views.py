a080a6e95420e08ea59ac3908ec35695e1898905
Kyle Chivers <chiverskyle@gmail.com>
2017-07-26 19:28:14 -0700

    successfully passing post object to post template

--- posts/views.py
+++ posts/views.py
@@ -7,9 +7,9 @@ from .models import Post, Category
 # Create your views here.
 
 def post_page(request, post_slug, category_slug):
-	post = get_object_or_404(Post, post_slug=slug, category_slug=category.slug)
+	post = get_object_or_404(Post, slug=post_slug, category__slug=category_slug)
 	return render(request, 'posts/post_page.html', {'post':post})
 
-def category_page(request, slug):
-	posts = Post.objects.order_by('-pub_date')
-	return render(request, 'posts/post_page.html', {'post':post})
+def category_page(request, category_slug):
+	posts = Post.objects.filter(category__slug=category_slug).order_by('-pub_date')
+	return render(request, 'posts/category_page.html', {'post':post})

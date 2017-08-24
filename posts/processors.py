108fbe18778fbc7b8445e0c2cb7ecbff63776c89
Kyle Chivers <chiverskyle@gmail.com>
2017-08-23 18:54:08 -0700

    added post object to context processor

--- posts/processors.py
+++ posts/processors.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from .models import Category
+from .models import Category, Post
 
 def category_context(request):
-	category = Category.objects.order_by('title')
-	return {'category':category}
+	category_cont = Category.objects.order_by('title')
+	posts_context = Post.objects.order_by('-pub_date')
+	return {'category_cont':category_cont, 'posts_context':posts_context}

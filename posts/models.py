2d83f87d9d117a2534a140f5c9b964e9a705e718
Kyle Chivers <chiverskyle@gmail.com>
2017-08-29 17:25:20 -0700

    images now working due to settings.py mods

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.template.defaultfilters import slugify
+from filer.fields.image import FilerImageField
+from filer.fields.file import FilerFileField
 
 # Create your models here.
 
@@ -11,6 +13,7 @@ class Category(models.Model):
 	title = models.CharField(max_length=100, db_index=True, unique=True)
 	meta_desc = models.CharField(max_length=200, blank=True)
 	header = models.CharField(max_length=200, blank=True)
+	featured_image = FilerImageField(null=True, blank=True, related_name="cat_featured_image")
 	body = models.TextField(blank=True, default='')
 	slug = models.SlugField(max_length=100, db_index=True, blank=True, unique=True)
 
@@ -30,6 +33,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=250, unique=True)
 	meta_desc = models.CharField(max_length=200, blank=True)
 	header = models.CharField(max_length=200, blank=True)
+	featured_image = FilerImageField(null=True, blank=True, related_name="post_featured_image")
 	pub_date = models.DateTimeField()
 	body = models.TextField(blank=True, default='')
 	slug = models.SlugField(max_length=100, blank=True, unique=True)

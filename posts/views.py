e78461d2f6145e4ce53c54a090de4f90b513efc8
Kyle Chivers <chiverskyle@gmail.com>
2017-09-03 22:22:22 -0700

    included pagination functionality

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -13,4 +14,14 @@ def post_page(request, post_slug, category_slug):
 def category_page(request, category_slug):
 	posts = Post.objects.filter(category__slug=category_slug).order_by('-pub_date')
 	cat_obj = get_object_or_404(Category, slug=category_slug)
-	return render(request, 'posts/category_page.html', {'posts':posts, 'cat_obj':cat_obj})
+	page = request.GET.get('page', 1)
+	paginator = Paginator(posts, 4)
+
+	try:
+		post_paginate = paginator.page(page)
+	except PageNotAnInteger:
+		post_paginate = paginator.page(1)
+	except EmptyPage:
+		post_paginate = paginator.page(paginator.num_pages)
+
+	return render(request, 'posts/category_page.html', {'posts':posts, 'cat_obj':cat_obj, 'post_paginate':post_paginate})

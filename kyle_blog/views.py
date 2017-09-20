a89a2353fe12f320a89819a4d8345e62dcaab43a
Kyle Chivers <chiverskyle@gmail.com>
2017-09-19 20:02:10 -0700

    added 404 handling and template

--- kyle_blog/views.py
+++ kyle_blog/views.py
@@ -2,7 +2,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 from posts.models import Post, Category
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def home(request):
 	posts = Post.objects.order_by('-pub_date')
 	return render(request, 'home.html', {'posts':posts})
+
+def handler404(request):
+	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
